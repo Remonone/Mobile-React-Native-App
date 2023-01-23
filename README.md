419a6634d5bb320cfbfae52c8e917d3eb39fe552
Remonone <50263589+Remonone@users.noreply.github.com>
2023-01-23 23:48:28 +0200

    Trying out react native

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Mobile-React-Native-App
+
+## Tech Stack
+
+* React Native
+* MongoDB
+* Spring Boot
+* Amazon S3
+* WebSocket
+
+## Design Reference
+
+Link to design: [here](https://www.figma.com/community/file/1189554935999340910)
--- app/client/App.tsx
+++ app/client/App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 export default function App() {
+  const [switcher, onSwitch] = useState(false)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TouchableHighlight style={switcher ? styles.fade : styles.main} onPress={() => onSwitch(true)}>
+          <>
+            <Text>This is my test text!</Text>
+            <StatusBar style="auto" />
+          </>
+        </TouchableHighlight>
+      </View> 
+      <View style={ switcher ? styles.main : styles.fade}>
+        <Text>This is main Menu!</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'relative'
   },
+  main: {
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fade: {
+    zIndex: -1,
+    display: 'none'
+  }
 });
